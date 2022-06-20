1495f2cd30ffde545dc1e1dbc0a52e7f78c0ba29
Lcevallos <107563462+Lcevallos@users.noreply.github.com>
2022-06-20 09:34:25 -0500

    Update process.php

--- process.php
+++ process.php
@@ -160,7 +160,7 @@ $json = json_decode($responseData, true);
 <div class="container">
 	<div class="row">
 		<div class="col-md-12">
-			<img src="../imagenes/logo-datafast.png">
+			<img src="../imagenes/logo-datafast.png" alt="foto">
 		</div>
 		<div class="col-md-12">
 		<h1>Portal de compras</h1>
@@ -183,7 +183,7 @@ $json = json_decode($responseData, true);
 		</div>
 		<div class="row">
 		<div class="col-md-12 text-center">
-		<!-- <img src="../imagenes/marcas.png"> -->
+		
 		</div>
 	</div>
 	<p>Powered by <a href="http://www.datafast.com.ec/" target="_blank">Datafast</a></p>	
